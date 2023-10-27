de84bfa3e4ab6f8f65f4697bd07875fa10c27cbf
arvidbt <arvid.bergman.thorn@cygni.se>
2023-10-27 11:40:30 +0200

    update

---
+++ src/components/Icons.tsx
@@ -0,0 +1,13 @@
+import pkgTfi from 'react-icons/tfi/index'
+import pkgFa from 'react-icons/fa/index'
+import pkgBs from 'react-icons/bs/index'
+
+const { FaGithub } = pkgFa;
+const { TfiLinkedin } = pkgTfi;
+const { BsInfoSquare } = pkgBs;
+
+export const Icons = {
+    github: FaGithub,
+    linkedin: TfiLinkedin,
+    info: BsInfoSquare
+};
